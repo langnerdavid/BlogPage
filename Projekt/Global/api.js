a1145c067a43000e38e5edafebff25d200b9cf65
Tarek Bürner <tarekle47@web.de>
2023-07-13 16:24:23 +0200

    PostPreview API anbindung

--- Projekt/Global/api.js
+++ Projekt/Global/api.js
@@ -1,7 +1,7 @@
 const apiKey = "fmkmna4j";
 let groupKey = apiKey;
 
-console.log(getUsers());
+//console.log(getUsers());
 
 /*const testUser ={
     username: "testUser",
@@ -13,6 +13,37 @@ console.log(getUsers());
 }
 
 loginUser("testUser", "testUser12");*/
+const encodeTest = btoa('testUser:testUser12');
+const authHeaderTest = `Basic ${encodeTest}`;
+
+
+let sectionTitle1 = "This is 1. test section";
+
+const textContent = {
+    __type: "text",
+    data: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati adipisci saepe neque atque labore repellat asperiores voluptate corrupti consectetur ab! At, culpa quas optio quam ducimus libero repudiandae facere minima?",
+           
+}
+const imageContent = {
+    __type: "img",
+    url: "https://images4.alphacoders.com/115/thumb-1920-115716.jpg",
+    caption: "Ein Test Bild"
+}
+const objSection1 = {
+    sectionTitle: sectionTitle1,
+    content: [textContent, imageContent]
+}
+const testPost ={
+    title: "This is a test post",
+    content: [textContent, imageContent],
+    sections: [objSection1]
+}
+
+
+
+//postPost(testPost);
+//console.log(getOnePost('c461c844-890e-48f6-b7f8-9facc71f81cc'));
+
 
 async function getUsers() {
     try {
@@ -177,7 +208,8 @@ async function getPosts() {
             }
         );
         const data = await response.text();
-        console.log(data);
+        const jsonData = JSON.parse(data);
+        return jsonData;
         
     } catch (error) {
         console.error(error);
@@ -192,6 +224,7 @@ async function getOnePost(postid) {
             {
                 method: 'GET',
                 headers: {
+                    'Content-Type': 'application/json',
                     'group-key': groupKey
                 }
             }
@@ -205,13 +238,15 @@ async function getOnePost(postid) {
     }
 }
 
-async function getOnePost(post) {
+async function postPost(post) {
     try {
         const response = await fetch(
             'https://lukas.rip/api/posts',
             {
                 method: 'POST',
                 headers: {
+                    'Authorization': authHeaderTest,
+                    'Content-Type': 'application/json',
                     'group-key': groupKey
                 },
                 body: JSON.stringify(post)
--- Projekt/Index/index.js
+++ Projekt/Index/index.js
@@ -1,3 +1,59 @@
 const article1 = document.getElementById('article-1');
+const articles = document.getElementsByClassName('article');
 
 article1.addEventListener('click', ()=>{window.open("../Articles/article.html", "_self");});
+let lastTenPosts;
+let content;
+let contentImgaeUrl;
+
+getPosts().then((res)=>{
+    lastTenPosts = res;
+    console.log(lastTenPosts[0]);
+    for(i=0; i<lastTenPosts[0].content.length; i++){
+        if(lastTenPosts[0].content[i].data){
+            content = lastTenPosts[0].content[i].data;
+            continue;
+        }else{
+            contentImgaeUrl = lastTenPosts[0].content[i].url;
+            continue;
+        }
+    }
+    setArticlepreview(0);
+    
+}).catch(error => {
+    console.error(error);
+  });
+
+//
+
+function setArticlepreview(i){
+    articles[i].getElementsByTagName('h2')[0].innerHTML = lastTenPosts[i].title;
+    articles[i].getElementsByClassName('article-publishing-date')[0].innerHTML = formatTimeSinceCreation(lastTenPosts[i].createdAt);
+    articles[i].getElementsByClassName('article-publisher')[0].innerHTML = lastTenPosts[i].username;  
+    articles[i].getElementsByClassName('article-sub-text-p')[0].innerHTML = content;  
+    articles[i].getElementsByTagName('img')[0].src = contentImgaeUrl;    
+}
+
+function formatTimeSinceCreation(createdAt) {
+  const now = new Date();
+  const createdDate = new Date(createdAt);
+  const timeDiff = now - createdDate;
+
+  // Anzahl der vergangenen Minuten berechnen
+  const minutes = Math.floor(timeDiff / (1000 * 60));
+
+  if (minutes < 60) {
+    return 'Vor ' + minutes + (minutes === 1 ? ' Minute' : ' Minuten');
+  }
+
+  // Anzahl der vergangenen Stunden berechnen
+  const hours = Math.floor(timeDiff / (1000 * 60 * 60));
+
+  if (hours < 24) {
+    return 'Vor ' + hours + (hours === 1 ? ' Stunde' : ' Stunden');
+  }
+
+  // Anzahl der vergangenen Tage berechnen
+  const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+  return 'Vor ' + days + (days === 1 ? ' Tag' : ' Tage');
+}
