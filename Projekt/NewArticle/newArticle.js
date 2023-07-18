fc9fd4e6127619034d5af6981f45e4f25581d91b
langnerdavid <langner.david@outlook.com>
2023-07-18 17:33:42 +0200

    Added a cool blog entry page

--- Projekt/NewArticle/newArticle.js
+++ Projekt/NewArticle/newArticle.js
@@ -1,74 +1,15 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const addSectionButton = document.getElementById('add-section-button');
-    const sectionsContainer = document.getElementById('sections-container');
-  
-    let sectionCount = 1;
-  
-    addSectionButton.addEventListener('click', function() {
-      const sectionId = `section-${sectionCount}`;
-  
-      const sectionDiv = document.createElement('div');
-      sectionDiv.id = sectionId;
-  
-      const sectionHeadingLabel = document.createElement('label');
-      sectionHeadingLabel.textContent = `Section ${sectionCount} Heading:`;
-  
-      const sectionHeadingInput = document.createElement('input');
-      sectionHeadingInput.type = 'text';
-      sectionHeadingInput.name = `section-heading-${sectionCount}`;
-  
-      const sectionTextContentLabel = document.createElement('label');
-      sectionTextContentLabel.textContent = `Section ${sectionCount} Text Content:`;
-  
-      const sectionTextContentTextarea = document.createElement('textarea');
-      sectionTextContentTextarea.name = `section-text-content-${sectionCount}`;
-      sectionTextContentTextarea.rows = '4';
-  
-      const sectionImageContentLabel = document.createElement('label');
-      sectionImageContentLabel.textContent = `Section ${sectionCount} Image Content:`;
-  
-      const sectionImageContentTextarea = document.createElement('textarea');
-      sectionImageContentTextarea.name = `section-image-content-${sectionCount}`;
-      sectionImageContentTextarea.rows = '1';
-  
-      const removeSectionButton = document.createElement('button');
-      removeSectionButton.type = 'button';
-      removeSectionButton.textContent = `Remove Section ${sectionCount}`;
-      removeSectionButton.addEventListener('click', function() {
-        sectionDiv.remove();
-        renumberSections();
-      });
-  
-      sectionDiv.appendChild(sectionHeadingLabel);
-      sectionDiv.appendChild(sectionHeadingInput);
-      sectionDiv.appendChild(sectionTextContentLabel);
-      sectionDiv.appendChild(sectionTextContentTextarea);
-      sectionDiv.appendChild(sectionImageContentLabel);
-      sectionDiv.appendChild(sectionImageContentTextarea);
-      sectionDiv.appendChild(removeSectionButton);
-  
-      sectionsContainer.appendChild(sectionDiv);
-  
-      sectionCount++;
-      renumberSections();
+const addSectionButton = document.getElementById('add-section-button');
+const sectionList = document.getElementById('section-list');
+const sectionTemplate = document.getElementById('section-template');
+
+addSectionButton.addEventListener('click', () => {
+    const clone = sectionTemplate.content.cloneNode(true);
+    const sectionWrapper = clone.querySelector('.section-wrapper');
+
+    sectionList.appendChild(clone);
+
+    const removeSectionButton = sectionWrapper.querySelector('.remove-section-button');
+    removeSectionButton.addEventListener('click', function() {
+        sectionWrapper.remove();
     });
-  
-    function renumberSections() {
-      const sectionDivs = sectionsContainer.querySelectorAll('div[id^="section-"]');
-      sectionCount = 1;
-  
-      sectionDivs.forEach(function(sectionDiv) {
-        const sectionId = sectionDiv.id;
-        const sectionHeadingLabel = sectionDiv.querySelector(`label[for="${sectionId}"]`);
-        const sectionTextContentLabel = sectionDiv.querySelector(`label[for="${sectionId}"] + textarea`);
-        const sectionImageContentLabel = sectionDiv.querySelector(`label[for="${sectionId}"] + textarea + label`);
-  
-        sectionHeadingLabel.textContent = `Section ${sectionCount} Heading:`;
-        sectionTextContentLabel.textContent = `Section ${sectionCount} Text Content:`;
-        sectionImageContentLabel.textContent = `Section ${sectionCount} Image Content:`;
-  
-        sectionCount++;
-      });
-    }
-  });
-  
+});
