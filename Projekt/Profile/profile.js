33d9a1dfc7c7faaf15711b07a46f33e40d9e21e4
langnerdavid <langner.david@outlook.com>
2023-07-18 14:39:25 +0200

    Username cant be edited

--- Projekt/Profile/profile.js
+++ Projekt/Profile/profile.js
@@ -19,26 +19,20 @@ function toggleEdit() {
   
 function saveData() {
     var displayName = document.getElementById('display-name').innerText;
-    var username = document.getElementById('username').innerText;
     var password = document.getElementById('password').innerText;
     var description = document.getElementById('description').innerText;
 
     localStorage.setItem('display-name', displayName);
-    localStorage.setItem('username', username);
     localStorage.setItem('password', password);
     localStorage.setItem('description', description);
 }
   
 var displayName = localStorage.getItem('display-name');
-var username = localStorage.getItem('username');
 var password = localStorage.getItem('password');
 var description = localStorage.getItem('description');
   
 if (displayName) {
     document.getElementById('display-name').innerText = displayName;
-}
-if (username) {
-    document.getElementById('username').innerText = username;
 }
 if (password) {
     document.getElementById('password').innerText = password;
